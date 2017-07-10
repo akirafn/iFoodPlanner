9577f2155040d1e9e4c64dd4a7810440bbfa2f88
akirafn <flavio.nakahara@gmail.com>
2017-07-10 18:18:44 -0300

    Barra de navegacao

---
+++ iFoodPlanner/ViewController/VCReceitaItem.h
@@ -0,0 +1,13 @@
+//
+//  VCReceitaItem.h
+//  iFoodPlanner
+//
+//  Created by Flavio Akira Nakahara on 7/10/17.
+//  Copyright © 2017 Flavio Akira Nakahara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCReceitaItem : UIViewController
+
+@end
---
+++ iFoodPlanner/ViewController/VCReceitaItem.m
@@ -0,0 +1,37 @@
+//
+//  VCReceitaItem.m
+//  iFoodPlanner
+//
+//  Created by Flavio Akira Nakahara on 7/10/17.
+//  Copyright © 2017 Flavio Akira Nakahara. All rights reserved.
+//
+
+#import "VCReceitaItem.h"
+
+@interface VCReceitaItem ()
+
+@end
+
+@implementation VCReceitaItem
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

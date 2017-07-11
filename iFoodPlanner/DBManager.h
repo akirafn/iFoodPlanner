1e31148f58b5a1978446ae8a81fa7b5ec16fd6ab
akirafn <flavio.nakahara@gmail.com>
2017-07-11 18:11:27 -0300

    Base de dados SQLite para receitas, inclusao de base no projeto, preparacao de telas

---
+++ iFoodPlanner/DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  iFoodPlanner
+//
+//  Created by Flavio Akira Nakahara on 7/11/17.
+//  Copyright © 2017 Flavio Akira Nakahara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
---
+++ iFoodPlanner/DBManager.m
@@ -0,0 +1,13 @@
+//
+//  DBManager.m
+//  iFoodPlanner
+//
+//  Created by Flavio Akira Nakahara on 7/11/17.
+//  Copyright © 2017 Flavio Akira Nakahara. All rights reserved.
+//
+
+#import "DBManager.h"
+
+@implementation DBManager
+
+@end
